f0f66eee8baa2cc93ed2fea3d13ed66364807e40
mintyconey <137746367+mintyconey@users.noreply.github.com>
2024-03-07 17:40:20 -0800

    Update publishFeedGenerators.ts

--- feed-generator/publishFeedGenerators.ts
+++ feed-generator/publishFeedGenerators.ts
@@ -9,7 +9,7 @@ const publishAll = async () => {
   // login
   const handle = `${process.env.BLUESKY_HANDLE}`
   const password = `${process.env.BLUESKY_APP_PASSWORD}`
-  const agent = new AtpAgent({ service: 'https://bsky.network' })
+  const agent = new AtpAgent({ service: 'https://bsky.social' })
   await agent.login({ identifier: handle, password })
 
   let configs = require("./configs.json")
